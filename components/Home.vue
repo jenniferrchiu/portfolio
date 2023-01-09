8dcc91711ce78e3d302b4b627a68cb8ff65d10f3
jenniferrchiu <69395914+jenniferrchiu@users.noreply.github.com>
2023-01-09 12:06:10 -0500

    added content

---
+++ components/Home.vue
@@ -0,0 +1,26 @@
+            I am a student at Columbia University studying computer science and
+            linguistics. I'm interested in computational linguistics, and I
+            also write.
+        </p>
+
+        <p>
+            You can see some of my writing <a href="https://jenniferchiu.carrd.co" target="_blank">here</a>.
+        </p>
+
+        <h2>Poetry</h2>
+            <ul>
+                <li>"Heritage Unwound," Rust + Moth <a href="https://rustandmoth.com/work/heritage-unwound/">(x)</a></li>
+                <li>"Maps," Pidgeonholes <a href="http://pidgeonholes.com/2021/03/maps/">(x)</a></li>
+            </ul>
+        <h2>Prose</h2>
+            <ul>
+                <li>"Time Lapse," wildness <a href="https://readwildness.com/26/chiu-time">(x)</a></li>
+                <li>"Supermarket Peaches," Hobart Pulp <a href="https://www.hobartpulp.com/web_features/crunchy-peaches">(x)</a></li>
+            </ul>
+        <h3>Edits</h3>
+            <ul>
+                <li>[sub]liminal Magazine <a href="https://readsubliminal.wixsite.com/readsubliminal">(x)</a></li>
+                <li>Dishsoap Quarterly <a href="https://dishsoapquarterly.com">(x)</a></li>
+            </ul>
+    </div>
+</template>
